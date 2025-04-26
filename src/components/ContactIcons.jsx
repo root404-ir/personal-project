27db014d72b0ab2872a0ccf3b53204be548d29c5
root404-ir <mohammadrg360@gmail.com>
2025-04-26 16:58:46 +0330

    add gsap for timline animation

--- src/components/ContactIcons.jsx
+++ src/components/ContactIcons.jsx
@@ -4,25 +4,37 @@ import { IoMdCall } from "react-icons/io";
 import { PiGithubLogoBold } from "react-icons/pi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 const ContactIcons = () => {
     const [contactMenuOpen, setContactMenuOpen] = useState(false)
-    const handleClick = () => {
-        setContactMenuOpen(!contactMenuOpen)
-    }
+
+    useEffect(() => {
+        let tl = gsap.timeline()
+        if (contactMenuOpen) {
+            tl.to('.telegram', { x: 100 })
+            tl.to('.call', { x: 200 })
+            tl.to('.github', { x: 300 })
+        } else {
+            tl.to('.telegram', { x: 0 })
+            tl.to('.call', { x: 0 })
+            tl.to('.github', { x: 0 })
+        }
+    }, [contactMenuOpen])
     return (
         <>
             <div className="absolute left-0">
-                <div className={`bg-white ${contactMenuOpen && 'translate-x-20'} absolute left-1 top-1  flex justify-center items-center text-black rounded-full p-2`}>
+                <div className={`bg-white telegram absolute left-1 top-1  flex justify-center items-center text-black rounded-full p-2`}>
                     <FaTelegramPlane className="text-2xl" />
                 </div>
-                <div className={`bg-white ${contactMenuOpen && 'translate-x-32'} absolute left-1 top-1 flex justify-center items-center text-black rounded-full p-2`}>
+                <div className={`bg-white call  absolute left-1 top-1 flex justify-center items-center text-black rounded-full p-2`}>
                     <IoMdCall className="text-2xl" />
                 </div>
-                <div className={`bg-white ${contactMenuOpen && 'translate-x-44'} absolute left-1 top-1 flex justify-center items-center text-black rounded-full p-2`}>
+                <div className={`bg-white github  absolute left-1 top-1 flex justify-center items-center text-black rounded-full p-2`}>
                     <PiGithubLogoBold className="text-2xl" />
                 </div>
-                <div onClick={handleClick} className="bg-white cursor-pointer absolute left-0 flex justify-center items-center text-black rounded-full p-2">
+                <div onClick={() => setContactMenuOpen(!contactMenuOpen)} className="bg-white cursor-pointer absolute left-0 flex justify-center items-center text-black rounded-full p-2">
                     {contactMenuOpen ? <AiOutlineClose className="text-3xl" /> : <MdContactPhone className="text-3xl" />}
                 </div>
             </div >
@@ -31,3 +43,8 @@ const ContactIcons = () => {
 }
 
 export default ContactIcons
+
+
+// ${contactMenuOpen && 'translate-x-20'}
+// ${contactMenuOpen && 'translate-x-32'}
+// ${contactMenuOpen && 'translate-x-44'}
