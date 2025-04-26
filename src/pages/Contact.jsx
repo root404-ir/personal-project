7ba100530f09f365a301ad45d6db3570010b2a2c
root404-ir <mohammadrg360@gmail.com>
2025-04-26 14:27:06 +0330

    send message to user email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,26 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup'
 import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2';
 const Contact = () => {
 
   const handleSubmit = (values, { resetForm }) => {
-    emailjs.send('service_tgvrlqj', 'template_m5458kq', values, 'DYfc28KrSjaXAHSRr')
-      .then(response => {
-        console.log(response)
+    const templateParams = {
+      name: values.name,
+      email: values.email,
+      message: values.message,
+      userEmail: values.email
+    }
+    emailjs.send('service_tgvrlqj', 'template_m5458kq', templateParams, 'DYfc28KrSjaXAHSRr')
+      .then(() => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "ارسال موفق",
+          text: "با تشکر از پیام ارزشمندتون , پیام شما به ایمیل بنده ارسال شده است",
+          showConfirmButton: true,
+          timer: 1500
+        });
         resetForm()
       })
   }
@@ -18,36 +32,40 @@ const Contact = () => {
   })
 
   return (
-    <div>
-      <h1>تماس با من</h1>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          message: ''
-        }}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <div>
-            <label htmlFor="name">نام</label>
-            <Field type="text" id="name" name="name" />
-            <ErrorMessage name='name' component="div" className='text-red-500' />
-          </div>
-          <div>
-            <label htmlFor="email">ایمیل</label>
-            <Field type="text" id="email" name="email" />
-            <ErrorMessage name='email' component="div" className='text-red-500' />
-          </div>
-          <div>
-            <label htmlFor="message">پیام</label>
-            <Field type="text" id="message" name="message" />
-            <ErrorMessage name='message' component="div" className='text-red-500' />
-          </div>
-          <button type='submit'>ارسال پیام</button>
-        </Form>
-      </Formik>
+    <div className='flex flex-col items-center mt-10'>
+      <div className='bg-white text-black px-10 py-5 w-96 rounded-lg'>
+        <h1 className='text-center text-3xl'>تماس با من</h1>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            message: ''
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className='mt-10'>
+            <div className='flex flex-col'>
+              <label htmlFor="name">نام</label>
+              <Field type="text" id="name" name="name" className='border border-gray-400 rounded-sm' />
+              <ErrorMessage name='name' component="div" className='text-red-500' />
+            </div>
+            <div className='flex flex-col mt-5'>
+              <label htmlFor="email">ایمیل</label>
+              <Field type="text" id="email" name="email" className='border border-gray-400 rounded-sm' />
+              <ErrorMessage name='email' component="div" className='text-red-500' />
+            </div>
+            <div className='flex flex-col mt-5'>
+              <label htmlFor="message">پیام</label>
+              <Field type="text" id="message" name="message" className='border border-gray-400 rounded-sm' />
+              <ErrorMessage name='message' component="div" className='text-red-500' />
+            </div>
+            <div className='flex justify-center'>
+              <button type='submit' className='mt-5 bg-green-700 px-5 py-2 rounded-md text-white cursor-pointer hover:-translate-y-2 hover:bg-green-500  transition-all'>ارسال پیام</button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   )
 }
