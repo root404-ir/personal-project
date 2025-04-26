45ffd6282040c66a822bab6fd41a6e41446d37ad
root404-ir <mohammadrg360@gmail.com>
2025-04-26 16:29:16 +0330

    add recaptcha

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,26 @@ import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup'
 import emailjs from 'emailjs-com'
 import Swal from 'sweetalert2';
+import { useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import ContactIcons from '../components/ContactIcons';
+
+const SITE_KEY = '6Le_biUrAAAAAPHoz2QuQwo-gQ_1sZyh3jOI3rRi'
+
 const Contact = () => {
+  const [recaptchaValue, setRecaptchaValue] = useState(null)
 
   const handleSubmit = (values, { resetForm }) => {
+    if (!recaptchaValue) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "لطفا تیک من ربات نیستم را بزنید",
+        showConfirmButton: true,
+        timer: 3500
+      });
+      return
+    }
     const templateParams = {
       name: values.name,
       email: values.email,
@@ -22,6 +39,7 @@ const Contact = () => {
           timer: 1500
         });
         resetForm()
+        setRecaptchaValue(null)
       })
   }
 
@@ -33,6 +51,7 @@ const Contact = () => {
 
   return (
     <div className='flex flex-col items-center mt-10'>
+      <ContactIcons />
       <div className='bg-white text-black px-10 py-5 w-96 rounded-lg'>
         <h1 className='text-center text-3xl'>تماس با من</h1>
         <Formik
@@ -59,6 +78,12 @@ const Contact = () => {
               <label htmlFor="message">پیام</label>
               <Field type="text" id="message" name="message" className='border border-gray-400 rounded-sm' />
               <ErrorMessage name='message' component="div" className='text-red-500' />
+            </div>
+            <div className='mt-10'>
+              <ReCAPTCHA
+                sitekey={SITE_KEY}
+                onChange={(value) => setRecaptchaValue(value)}
+              />
             </div>
             <div className='flex justify-center'>
               <button type='submit' className='mt-5 bg-green-700 px-5 py-2 rounded-md text-white cursor-pointer hover:-translate-y-2 hover:bg-green-500  transition-all'>ارسال پیام</button>
